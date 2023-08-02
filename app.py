0990549c338edce9f95c529fcb52ab16d7292b1e
Yash Surange <88226034+ysurs@users.noreply.github.com>
2023-08-03 00:05:24 +0530

    raw text, chunking, creating vectore store

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
 import streamlit as st
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
 
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader=PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+=page.extract_text()
+    return text
 
+def get_text_chunks(text):
+    text_splitter=CharacterTextSplitter(separator="\n",chunk_size=1000,chunk_overlap=200,length_function=len)
+    chunks=text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embeddings=HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore=FAISS.from_texts(texts=text_chunks,embedding=embeddings)
+    return vectorstore
+    
 def main():
     
     load_dotenv()
@@ -12,8 +33,19 @@ def main():
     
     with st.sidebar:
      st.subheader("Your documents")
-     st.file_uploader("Upload your pdfs here and click on 'Process'")
-     st.button("Process")
+     pdf_docs=st.file_uploader("Upload your pdfs here and click on 'Process'",accept_multiple_files=True)
+     
+     if st.button("Process"):
+         with st.spinner("Processing"): # while processing of documents is happening, it gives 
+            # get pdf text
+            raw_text=get_pdf_text(pdf_docs) # will get all pdf text in a single string
+            
+            # get chunks of text
+            text_chunks=get_text_chunks(raw_text) # will get a list of all text chunks
+            
+            # create vector store
+            vectorstore=get_vector_store(text_chunks)
+         
      
 
 if __name__ == '__main__':
